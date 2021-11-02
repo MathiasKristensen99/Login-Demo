6003233710c4439e55a812c7219e6ce76cb4ab56
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-11-02 15:25:29 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module')
+      .then(m => m.AuthModule)
+  },
+  {
+    path: 'pets',
+    loadChildren: () => import('./admin-pets/admin-pets.module')
+      .then(m => m.AdminPetsModule)
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('./todos/todos.module')
+      .then(m => m.TodosModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
