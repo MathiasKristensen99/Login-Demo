631638daf00113ff5bdf700d1965dfbe7a01a649
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-11-10 15:03:49 +0100

    Added some stuff

--- src/app/products/shared/products.service.ts
+++ src/app/products/shared/products.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Product} from "./product.model";
 import {ProductList} from "./product-list.model";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private productsApi = environment.api + '/products'
 
   constructor(private _http: HttpClient) { }
 
   getProducts(): Observable<ProductList> {
-    return this._http.get<ProductList>('https://localhost:5001/api/products');
+    return this._http.get<ProductList>(this.productsApi);
+  }
+
+  getProduct(id: number): Observable<Product> {
+    return this._http.get<Product>(this.productsApi + '/' + id);
+    //return of({id: 1, name: 'Idiot'});
+  }
+
+  update(product: Product): Observable<Product> {
+    return this._http.put<Product>(this.productsApi + '/' + product.id, product);
   }
 }
